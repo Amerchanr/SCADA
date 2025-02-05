659978c90a5f1afa21e0fece71f854b75bcb0048
Amerchanr <amerchanr@unal.edu.co>
2025-02-04 23:13:02 -0500

    cuarto commit

--- Interfaz.py
+++ Interfaz.py
@@ -46,7 +46,6 @@ def Scada():
 #funcion ventana de registro
 
 def registro():
-    global caja_texto_usuario, caja_texto_contraseña, text_area
     ventana_registro=tk.Toplevel(ventana)
     ventana_registro.attributes('-fullscreen',1)
     ventana_registro.config(bg='white')
@@ -71,7 +70,6 @@ def registro():
         referencia= db.reference(f'usuarios/{texto_usuario}')
         name=caja_texto_usuario.get("1.0", tk.END).strip()
         if referencia.get():
-                print('el jugador ya esta registrado')
                 text_area.insert(tk.END,f'El Usuario {name}, ya se encuentra registrado')
         else:        
             text_area.delete("1.0", tk.END)    
@@ -84,15 +82,54 @@ def registro():
                       
     boton_registro=tk.Button(ventana_registro,text='registrarse',font=(fuentebotones),bg='#000000',fg='white',relief='raised',command=dataregist)
     boton_registro.place(x=500,y=600)
+
+def inicio_sesion():
+    ventana_inicio=tk.Toplevel(ventana)
+    ventana_inicio.attributes('-fullscreen',1)
+    ventana_inicio.config(bg='white')
+    iniciar= tk.Label(ventana_inicio,text='inicie sesión',font=(fuentetitbrick),bg='white',fg='black')
+    iniciar.place(x=200,y=20)
+    boton_volver=tk.Button(ventana_inicio,text='volver',font=(fuentebotonesventanas),bg='#000000',fg='white',relief='raised',command=ventana_inicio.destroy)
+    boton_volver.place(x=1180,y=20)
+    usuario=tk.Label(ventana_inicio,text='usuario',font=(fuenteusucontra),bg='white',fg='black')
+    usuario.place(x=500,y=220)
+    caja_texto_usuario = tk.Text(ventana_inicio, height=1, width=40)
+    caja_texto_usuario.place(x=475,y=310)
+    contraseña=tk.Label(ventana_inicio,text='contraseña',font=(fuenteusucontra),bg='white',fg='black')
+    contraseña.place(x=450,y=350)
+    caja_texto_contraseña = tk.Text(ventana_inicio, height=1, width=40)
+    caja_texto_contraseña.place(x=475,y=425)
     
+    text_area = tk.Text(ventana_inicio,height=3,width=50)
+    text_area.place(x=500,y=450)    
+    def inicio():
+        texto_usuario=caja_texto_usuario.get("1.0", tk.END).strip()
+        texto_contraseña=caja_texto_contraseña.get("1.0", tk.END).strip()
+        referencia= db.reference(f'usuarios/{texto_usuario}')
+        name=caja_texto_usuario.get("1.0", tk.END).strip()
+        dato_usuario=referencia.get()
+        contr_usua=dato_usuario.get('contraseña')
+        print(contr_usua)
+        print(texto_contraseña)
+        if (contr_usua== texto_contraseña):
+             
+             text_area.insert(tk.END,f'Hola {name}, Bienvenido desde la siguiente pestaña podrá observar lo referente a su sistema de alcantalillado')
+             def destruir_inicio():
+                  ventana_inicio.destroy
+             ventana_inicio.after(100000,destruir_inicio)
+             Scada()
+        else:
+             text_area.insert(tk.END,f'Hola {name}, el usuario o la contraseñaa estan incorrectos')
+    boton_sesion=tk.Button(ventana_inicio,text='iniciar sesion',font=(fuentebotones),bg='#000000',fg='white',relief='raised',command=inicio)
+    boton_sesion.place(x=500,y=550)    
 
 
 titulo = tk.Label(ventana,text='SCADA SISTEMA INTERNO\nDE ALCANTARILLADO',font=(fuentetitulo),bg='white',fg='black')
 titulo.place(x=0,y=40)
 registrarse= tk.Button(ventana,text='registrarse',font=fuentebotones,bg='#000000',fg='white',relief='raised',command=registro)
 registrarse.place(x=530,y=350)
-#multi_player= tk.Button(text='Multijugador',font=(fuentebotones),bg='#000000',fg='white',relief='raised',command=crear_ventana_multiplayer)
-#multi_player.place(x=510,y=440)
+but_inicio= tk.Button(text='Inicio de Sesión',font=(fuentebotones),bg='#000000',fg='white',relief='raised',command=inicio_sesion)
+but_inicio.place(x=480,y=440)
 #brick_Breaker= tk.Button(text='Brick Breacker',font=(fuentebotones),bg='#000000',fg='white',relief='raised',command=crear_ventana_brick)
 #brick_Breaker.place(x=490,y=530)
 salir= tk.Button(text='Salir',font=(fuentebotones),bg='#000000',fg='white',relief='raised',command=ventana.quit)
